a7e537b192d7e289b26e4d3df04d959360f28f61
Goran0302 <143317197+Goran0302@users.noreply.github.com>
2023-10-19 17:36:36 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+def factorial(n)
+    lf n ==0:
+        return 1
+    else:
+        return * factorial(n -1)
+        
+number = 5
+result = factorial (number)
+print(f"the factorial of (number) is {result}.")
